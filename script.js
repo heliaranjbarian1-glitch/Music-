fee51e8a81d83dccec280822bcfca39dab404b7b
heliaranjbarian1-glitch <heliaranjbarian1@gmail.com>
2026-07-19 14:40:13 +0330

    Create script.js

---
+++ script.js
@@ -0,0 +1,439 @@
+const songs = [
+
+{
+title:"The Fate of Ophelia",
+artist:"Taylor Swift",
+cover:"",
+link:""
+},
+
+{
+title:"Moth To A Flame",
+artist:"The Weeknd",
+cover:"",
+link:""
+},
+
+{
+title:"24/7",
+artist:"Arta",
+cover:"",
+link:""
+},
+
+{
+title:"Church",
+artist:"Chase Atlantic",
+cover:"",
+link:""
+},
+
+{
+title:"Heaven and Back",
+artist:"Chase Atlantic",
+cover:"",
+link:""
+},
+
+{
+title:"The Walls",
+artist:"Chase Atlantic",
+cover:"",
+link:""
+},
+
+{
+title:"Slow Down",
+artist:"Chase Atlantic",
+cover:"",
+link:""
+},
+
+{
+title:"Friends",
+artist:"Chase Atlantic",
+cover:"",
+link:""
+},
+
+{
+title:"Into It",
+artist:"Chase Atlantic",
+cover:"",
+link:""
+},
+
+{
+title:"Swim",
+artist:"Chase Atlantic",
+cover:"",
+link:""
+},
+
+{
+title:"Hunted",
+artist:"Chris Grey",
+cover:"",
+link:""
+},
+
+{
+title:"Cold Blooded",
+artist:"Chris Grey",
+cover:"",
+link:""
+},
+
+{
+title:"Bad For You",
+artist:"Chris Grey",
+cover:"",
+link:""
+},
+
+{
+title:"Enemies",
+artist:"Chris Grey",
+cover:"",
+link:""
+},
+
+{
+title:"Forbidden Fruit",
+artist:"Tommie",
+cover:"",
+link:""
+},
+  {
+title:"Sailor Song",
+artist:"Gigi Perez",
+cover:"",
+link:""
+},
+
+{
+title:"Funhouse",
+artist:"Chris Grey",
+cover:"",
+link:""
+},
+
+{
+title:"Let The World Burn",
+artist:"Chris Grey",
+cover:"",
+link:""
+},
+
+{
+title:"Meddle About",
+artist:"Chase Atlantic",
+cover:"",
+link:""
+},
+
+{
+title:"The Hills",
+artist:"The Weeknd",
+cover:"",
+link:""
+},
+
+{
+title:"Compass",
+artist:"The Neighbourhood",
+cover:"",
+link:""
+},
+
+{
+title:"If I Had You",
+artist:"Chris Grey",
+cover:"",
+link:""
+},
+
+{
+title:"Always Been You",
+artist:"Chris Grey",
+cover:"",
+link:""
+},
+
+{
+title:"Make The Angels Cry",
+artist:"Chris Grey",
+cover:"",
+link:""
+},
+
+{
+title:"Bring Me Back To Life",
+artist:"Chris Grey",
+cover:"",
+link:""
+},
+
+{
+title:"Heart Beat",
+artist:"Isabel LaRosa",
+cover:"",
+link:""
+},
+
+{
+title:"Home",
+artist:"Isabel LaRosa",
+cover:"",
+link:""
+},
+
+{
+title:"More Than Friends",
+artist:"Isabel LaRosa",
+cover:"",
+link:""
+},
+
+{
+title:"Butterflies",
+artist:"Isabel LaRosa",
+cover:"",
+link:""
+},
+
+{
+title:"Mine",
+artist:"Ari Abdul",
+cover:"",
+link:""
+},
+  {
+title:"Death Won't Do Us Part",
+artist:"Chris Grey & Ari Abdul",
+cover:"",
+link:""
+},
+
+{
+title:"Hurts So Good",
+artist:"Astrid S",
+cover:"",
+link:""
+},
+
+{
+title:"K",
+artist:"Cigarettes After Sex",
+cover:"",
+link:""
+},
+
+{
+title:"Wrong",
+artist:"Chris Grey",
+cover:"",
+link:""
+},
+
+{
+title:"Friend Zone",
+artist:"Arman Miladi",
+cover:"",
+link:""
+},
+
+{
+title:"Older",
+artist:"Isabel LaRosa",
+cover:"",
+link:""
+},
+
+{
+title:"Cry",
+artist:"Cigarettes After Sex",
+cover:"",
+link:""
+},
+
+{
+title:"Pistol",
+artist:"Cigarettes After Sex",
+cover:"",
+link:""
+},
+
+{
+title:"Heavenly",
+artist:"Cigarettes After Sex",
+cover:"",
+link:""
+},
+
+{
+title:"Sunsetz",
+artist:"Cigarettes After Sex",
+cover:"",
+link:""
+},
+
+{
+title:"Ocean Eyes",
+artist:"Billie Eilish",
+cover:"",
+link:""
+},
+
+{
+title:"Jadeye Yektarafe",
+artist:"Morteza Pashaei",
+cover:"",
+link:""
+},
+
+{
+title:"Yeki Hast",
+artist:"Morteza Pashaei",
+cover:"",
+link:""
+},
+
+{
+title:"Negarane Mani",
+artist:"Morteza Pashaei",
+cover:"",
+link:""
+},
+
+{
+title:"Asheghaye Be Esm",
+artist:"Morteza Pashaei",
+cover:"",
+link:""
+},
+  {
+title:"Asheghaye Be Esm 2",
+artist:"Morteza Pashaei",
+cover:"",
+link:""
+},
+
+{
+title:"Doin' Time",
+artist:"Lana Del Rey",
+cover:"",
+link:""
+},
+
+{
+title:"I Was Never There",
+artist:"The Weeknd",
+cover:"",
+link:""
+},
+
+{
+title:"Born To Die",
+artist:"Lana Del Rey",
+cover:"",
+link:""
+},
+
+{
+title:"Diet Mountain Dew",
+artist:"Lana Del Rey",
+cover:"",
+link:""
+},
+
+{
+title:"Say Yes To Heaven",
+artist:"Lana Del Rey",
+cover:"",
+link:""
+},
+
+{
+title:"Sange Saboor",
+artist:"Mohsen Chavoshi",
+cover:"",
+link:""
+},
+
+{
+title:"Summertime Sadness",
+artist:"Lana Del Rey",
+cover:"",
+link:""
+},
+
+{
+title:"Asre Paeizi",
+artist:"Morteza Pashaei",
+cover:"",
+link:""
+},
+
+{
+title:"Dandelions",
+artist:"Ruth B.",
+cover:"",
+link:""
+}
+
+];
+const songsContainer = document.getElementById("songsContainer");
+const searchInput = document.getElementById("searchInput");
+
+function createSongCard(song){
+
+return `
+<div class="song-card">
+
+<img class="cover" src="${song.cover || "https://placehold.co/300x300/111/ffffff?text=%E2%99%AA"}">
+
+<div class="song-info">
+
+<h3>${song.title}</h3>
+
+<p>${song.artist}</p>
+
+<a href="${song.link}" target="_blank" class="download-btn">
+Download
+</a>
+
+</div>
+
+</div>
+`;
+
+}
+
+function renderSongs(list){
+
+songsContainer.innerHTML="";
+
+list.forEach(song=>{
+songsContainer.innerHTML+=createSongCard(song);
+});
+
+}
+
+renderSongs(songs);
+
+searchInput.addEventListener("input",()=>{
+
+const value=searchInput.value.toLowerCase();
+
+const filtered=songs.filter(song=>
+
+song.title.toLowerCase().includes(value) ||
+
+song.artist.toLowerCase().includes(value)
+
+);
+
+renderSongs(filtered);
+
+});
